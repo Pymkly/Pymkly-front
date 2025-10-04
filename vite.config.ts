bbc26d83562ace71809926fa1223615fd3c9de9c
Pymkly <hrivonandrasana@gmail.com>
2025-10-04 20:29:37 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [tailwindcss(),react()],
   server: {
-    allowedHosts: ['vps-156d6086.vps.ovh.net']
+    allowedHosts: ['54.37.158.173']
   }
 })
